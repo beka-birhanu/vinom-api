961bfb8b2b7c842ee7eeda4247d7c9567d1da795
beka birhanu <beka.b.atomsa@gamil.com>
2024-12-18 21:52:39 +0300

    chore: privide server example on how to communicate udp and game server, #7

--- main.go
+++ main.go
@@ -1,16 +1,1 @@
 package main
-
-import (
-	"fmt"
-
-	maze "github.com/beka-birhanu/vinom-api/game/willson_maze"
-)
-
-func main() {
-	maz, _ := maze.New(10, 10)
-	fmt.Println(maz)
-	if maze.PopulateReward(maze.RewardModel{RewardOne: 1, RewardTwo: 5, RewardTypeProb: 0.9}, maz) != nil {
-		return
-	}
-	fmt.Println(maz)
-}
