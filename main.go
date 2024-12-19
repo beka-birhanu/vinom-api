ac746b84fd4d3534f722f2983c64b1b971362569
beka birhanu <beka.b.atomsa@gamil.com>
2024-12-19 10:51:23 +0300

    chore: add containerzation files

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/beka-birhanu/vinom-api/game/maze"
-)
+// build user data base
+// provid signin signup
+// provide start match making
+// provide match checking
+// provide public rsa key serving
+// privide https endpoint for all
 
 func main() {
-	maz, _ := maze.New(10, 10)
-	fmt.Println(maz)
-	if maze.PopulateReward(maze.RewardModel{RewardOne: 1, RewardTwo: 5, RewardTypeProb: 0.9}, maz) != nil {
-		return
-	}
-	fmt.Println(maz)
+	fmt.Println("server up and running...")
 }
