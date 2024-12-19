176dd3007b74151633bff2d53653d8bee69cb231
beka birhanu <beka.b.atomsa@gamil.com>
2024-12-19 10:51:23 +0300

    chore: add containerzation files

---
+++ identity/auth_middlware.go
@@ -0,0 +1,48 @@
+package identity
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	// ContextUserClaims is the key used to store user claims in the Gin context.
+	ContextUserClaims = "userClaims"
+)
+
+func Authoriz(ts TokenService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Retrieve the access token from the Authorization header.
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.Status(http.StatusUnauthorized) // No token found in the header.
+			c.Abort()
+			return
+		}
+
+		// Split the "Bearer" prefix from the token.
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+			c.Status(http.StatusUnauthorized) // Malformed Authorization header.
+			c.Abort()
+			return
+		}
+
+		// Extract the token part.
+		token := parts[1]
+
+		// Validate the token using the barrier service.
+		claims, err := ts.Decode(token)
+		if err != nil {
+			c.Status(http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		// Attach user claims to the request context for further use.
+		c.Set(ContextUserClaims, claims)
+		c.Next()
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+// build user data base
+// provid signin signup
+// provide start match making
+// provide match checking
+// provide public rsa key serving
+// privide https endpoint for all
+
+func main() {
+	fmt.Println("server up and running...")
+}
