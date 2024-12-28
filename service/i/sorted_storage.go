73d73c80a90d8d5ed62b2004f92d4feb58af8198
beka birhanu <beka.b.atomsa@gamil.com>
2024-12-28 09:33:07 +0300

    fix(matchmaking): fix storage of uuid in sorted storage, #10

--- service/i/sorted_storage.go
+++ service/i/sorted_storage.go
@@ -5,10 +5,10 @@ import "context"
 // SortedQueue defines an interface for managing sorted queues.
 type SortedQueue interface {
 	// Enqueue adds a member to the sorted queue with a given score.
-	Enqueue(ctx context.Context, queueKey string, score float64, member interface{}) error
+	Enqueue(ctx context.Context, queueKey string, score float64, member string) error
 
 	// DequeTops removes and retrieves up to `amount` members with the lowest scores from the queue.
-	DequeTops(ctx context.Context, queueKey string, amount int64) ([]interface{}, error)
+	DequeTops(ctx context.Context, queueKey string, amount int64) ([]string, error)
 
 	// Count returns the number of members in the sorted queue.
 	Count(ctx context.Context, queueKey string) int64
